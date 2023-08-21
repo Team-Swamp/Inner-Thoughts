96f9df7139aaaf84815693348ec53a20c0cdc772
bas-boop <dereusbas461@gmail.com>
2023-08-22 00:57:49 +0200

    Added damage to target of bullet

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -19,8 +19,7 @@ public sealed class PlayerShooting : MonoBehaviour
     private void Shoot()
     {
         var currentBullet = Instantiate(currentGunData.bullet, transform.position, transform.rotation); //todo: rotation laten kloppen met de riching dat de bullet op gaat. (als de bullets niet rond zijn)
-        var bulletRB = currentBullet.GetComponent<Rigidbody2D>();
-        bulletRB.velocity = ShootingDirection() * currentGunData.shootingPower;
+        currentBullet.GetComponent<Bullet>().ActiveBullet(ShootingDirection(), currentGunData.shootingPower, "Enemy");
         
         onShoot?.Invoke();
     }
