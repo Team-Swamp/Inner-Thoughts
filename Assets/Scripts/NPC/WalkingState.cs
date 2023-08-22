21312b9b3ced4ff46214782e8ab5d62c00cf18d0
rickdohmen <rickdohmen2003@gmail.com>
2023-08-22 16:56:36 +0200

    added a start to the walking state

--- Assets/Scripts/NPC/WalkingState.cs
+++ Assets/Scripts/NPC/WalkingState.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WalkingState : SmallEnemiesBaseState
 {
+    private Vector2 _currentWalkingPoint;
+    private bool _isDoneWalking;
     protected override void EnterState(SmallEnemiesStateMachine enemy)
     {
-        
+        IsValidToSwitch = true;
+
+        var random = Random.Range(0, enemy.WalkPoints.Length);
+        _currentWalkingPoint = enemy.WalkPoints[random].position;
+        enemy.Agent.SetDestination(_currentWalkingPoint);
     }
 
     protected override void ExitState(SmallEnemiesStateMachine enemy) { }
@@ -13,6 +20,9 @@ public class WalkingState : SmallEnemiesBaseState
     protected override void FixedUpdateState(SmallEnemiesStateMachine enemy) { }
     
 
-    protected override void UpdateState(SmallEnemiesStateMachine enemy) { }
+    protected override void UpdateState(SmallEnemiesStateMachine enemy)
+    {
+        if (currentPos == targetPos) enemy.SwitchState(enemy.idleState);
+    }
     
 }
