eb5ab6e999977e383027827ccbb24ad9292a3752
bas-boop <dereusbas461@gmail.com>
2023-08-22 15:27:47 +0200

    Setting stuff up

---
+++ Assets/Scripts/Framework/Rooms/RoomManger.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManger : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> doors;
+    [SerializeField] private List<GameObject> enemies;
+    private int _enemiesDeathAmount;
+
+    // alle deuren opnen
+    
+    // Alle deuren dicht doen
+    
+    // Enemy die dood gaat
+    
+    // Checken of alle enemies dood zijn
+    
+    
+}
