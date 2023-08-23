2bfd4954a4edb495dc21cc4fd63e9d15407d3741
bas-boop <dereusbas461@gmail.com>
2023-08-23 18:50:33 +0200

    Added bool that turns true when room is cleared

--- Assets/Scripts/Framework/Rooms/RoomManger.cs
+++ Assets/Scripts/Framework/Rooms/RoomManger.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class RoomManger : MonoBehaviour
+public sealed class RoomManger : MonoBehaviour
 {
     [SerializeField] private List<GameObject> doors;
     [SerializeField] private List<GameObject> enemies;
@@ -12,42 +11,36 @@ public class RoomManger : MonoBehaviour
     [SerializeField] private UnityEvent onRoomCleared = new UnityEvent();
     [SerializeField] private UnityEvent onRoomEntered = new UnityEvent();
     
+    public bool IsCleared { get; private set; }
     private int _enemiesDeathAmount;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(!col.gameObject.HasTag("Player")) return;
         
-        Debug.Log("a");
         onRoomEntered?.Invoke();
     }
 
-    // alle deuren opnen
-    // Alle deuren dicht doen
     public void SetDoorsStates(bool targetValue)
     {
         foreach (var door in doors)
         {
             door.SetActive(targetValue);
         }
-        Debug.Log("b");
     }
-    
-    // Enemy die dood gaat
+
     public void EnemyHasDied()
     {
-        Debug.Log("c");
         _enemiesDeathAmount++;
         EveryEnemiesIsDead();
     }
 
-    // Checken of alle enemies dood zijn
     private void EveryEnemiesIsDead()
     {
         if(_enemiesDeathAmount != enemies.Count) return;
-        
-        Debug.Log("d");
-        SetDoorsStates(true);
+
+        IsCleared = true;
+        SetDoorsStates(false);
         onRoomCleared?.Invoke();
     }
 }
