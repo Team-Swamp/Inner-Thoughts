e3de28a631a7088a7cd1274707d3df464debf04d
Tatum13 <tatumdv2001@gmail.com>
2023-08-24 21:38:12 +0200

    added camera prefab

--- Assets/Scripts/Framework/Camera/CameraController.cs
+++ Assets/Scripts/Framework/Camera/CameraController.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public sealed class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-
     [SerializeField] private Vector3 offSet;
     
-
-    private void LateUpdate()
-    {
-        transform.position = player.transform.position + offSet;
-    }
+    private void LateUpdate() => transform.position = player.transform.position + offSet;
 }
