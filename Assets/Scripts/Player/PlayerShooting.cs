75ecffb66a9c3a64d7bb287e8d771bb6daa55018
bas-boop <dereusbas461@gmail.com>
2023-08-25 14:45:17 +0200

    Added automeatic shooting

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -11,18 +11,37 @@ public sealed class PlayerShooting : MonoBehaviour
 
     private GameObject _bullet;
     private float _shootingPower;
+    private float _shootingDelay;
+    private float _currentShootDelay;
+    private bool _autoShooting;
 
-    private void Awake() => SetGunData(startingGunData);
+    private void Awake()
+    {
+        SetGunData(startingGunData);
+        _currentShootDelay = startingGunData.shootingDelay;
+    }
 
     private void Update()
     {
-        if(Input.GetButtonDown("Fire1")) Shoot();
+        _currentShootDelay -= Time.deltaTime;
+        if (_autoShooting && Input.GetButton("Fire1")) DelayedShooting();
+        else if (Input.GetButtonDown("Fire1")) DelayedShooting();
     }
 
     public void SetGunData(GunData targetData)
     {
         _bullet = targetData.bullet;
         _shootingPower = targetData.shootingPower;
+        _shootingDelay = targetData.shootingDelay;
+        _autoShooting = targetData.autoShooting;
+    }
+
+    private void DelayedShooting()
+    {
+        if (!(_currentShootDelay <= 0)) return;
+        
+        Shoot();
+        _currentShootDelay = _shootingDelay;
     }
 
     private void Shoot()
--- Assets/Scripts/ScriptableObjects/GunData.cs
+++ Assets/Scripts/ScriptableObjects/GunData.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ScriptableObject / Gun Data")]
 public sealed class GunData : ScriptableObject
 {
+    [Header("Standaard")]
     public GameObject bullet;
     [Range(0, 25)] public float shootingPower = 2;
+    [Range(0, 1)] public float shootingDelay;
+    
+    [Header("Automatic")]
+    public bool autoShooting;
 }
