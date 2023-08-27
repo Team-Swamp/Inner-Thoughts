c5833233e7eac5982046fea1edde06e3456f5bea
bas-boop <dereusbas461@gmail.com>
2023-08-27 02:44:25 +0200

    Inverted contact damage logic

--- Assets/Scripts/Player/ContactDamage.cs
+++ Assets/Scripts/Player/ContactDamage.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 public sealed class ContactDamage : MonoBehaviour
 {
     [SerializeField] private float damageNumber;
-
-    [SerializeField] private HealthData healthData;
-
+    
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.HasTag("Enemy")) healthData.TakeDamage(damageNumber);
+        if (col.gameObject.HasTag("Player")) col.GetComponent<HealthData>().TakeDamage(damageNumber);
     }
 }
